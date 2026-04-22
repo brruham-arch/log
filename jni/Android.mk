b4744ee89677269a91a46266f2513b9ba7d21169
brruham-arch <brruham@example.com>
2026-04-22 11:38:51 +0700

    fix: remove curl dependency

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,12 +1,28 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := keylogger
-LOCAL_SRC_FILES := main.cpp
-LOCAL_LDLIBS := -llog
-LOCAL_STATIC_LIBRARIES := curl
-LOCAL_CPPFLAGS := -std=c++17 -fvisibility=hidden
-include $(BUILD_SHARED_LIBRARY)
 
-# Import libcurl
-$(call import-module, curl)
+LOCAL_MODULE := keylog
+LOCAL_SRC_FILES := main.cpp
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../include
+
+LOCAL_CPPFLAGS := \
+    -std=c++17 \
+    -O2 \
+    -fPIC \
+    -fvisibility=hidden \
+    -ffunction-sections \
+    -fdata-sections \
+    -mthumb
+
+LOCAL_LDLIBS := \
+    -llog \
+    -lm \
+    -ldl
+
+LOCAL_LDFLAGS := \
+    -static-libstdc++ \
+    -Wl,--gc-sections
+
+include $(BUILD_SHARED_LIBRARY)
